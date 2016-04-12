c3c18804f25eaef2b93f1d06637c359af755c1a8
Afton Downey <aaike001@gmail.com>
2016-04-12 14:15:56 -0700

    display api results

--- app/routes/results.js
+++ app/routes/results.js
@@ -2,10 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    console.log(params);
+    //console.log("params" + params);
+    debugger;
     var url = 'http://api.nal.usda.gov/ndb/search/?format=json&q=' + params.food + '&max=25&offset=0&api_key=1JVOH21MhirKVtA1rmgjCFBpVyjHMV5U4mR1FGiO';
     return Ember.$.getJSON(url).then(function(responseJSON) {
-      return responseJSON.results;
+      console.log("response: " + responseJSON.list.item[0].ndbno);
+      return responseJSON.list.item;
+    }).fail(function(){
+              console.log("error");
     });
   }
 });
