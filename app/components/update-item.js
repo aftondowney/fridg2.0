8c0f6060c261fcd1655cc21af3987c876da8d2eb
Afton Downey <aaike001@gmail.com>
2016-04-12 09:16:56 -0700

    add update-item functionality and template

--- app/components/update-item.js
+++ app/components/update-item.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateItemForm: false,
+  actions: {
+    updateItemForm() {
+      this.set('updateItemForm', true);
+    },
+    update(item) {
+      var params = {
+        name: this.get('name'),
+        quantity: this.get('city'),
+        notes: this.get('notes'),
+        date: this.get('date'),
+      };
+      this.set('updateItemForm', false);
+      this.sendAction('update', rental, params);
+    }
+  }
 });
