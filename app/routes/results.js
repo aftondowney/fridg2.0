4bc64cd1be91e5673730fc1137bd2e5b47285552
jakemento <jakesteinberg@gmail.com>
2016-04-15 09:28:43 -0700

    Update results.js

--- app/routes/results.js
+++ app/routes/results.js
@@ -2,11 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    var url = 'http://api.nal.usda.gov/ndb/search/?format=json&q=' + params.food + '&max=25&offset=0&api_key=1JVOH21MhirKVtA1rmgjCFBpVyjHMV5U4mR1FGiO';
+    var url = 'http://api.nal.usda.gov/ndb/search/?format=json&q=' + params.food + '&offset=0&api_key=1JVOH21MhirKVtA1rmgjCFBpVyjHMV5U4mR1FGiO';
     return Ember.$.getJSON(url).then(function(responseJSON) {
       return responseJSON.list.item;
     }).fail(function(){
-              console.log("error");
+              alert("Your search did not return any results");
     });
   }
 });
